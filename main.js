1bd36333b0ea038d0b96f4599a91c9d1e788fc62
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-06-08 11:22:33 +0200

    initial version

---
+++ main.js
@@ -0,0 +1,186 @@
+/**
+ * Created by Samuel Gratzl on 04.09.2014.
+ */
+
+(function (LineUpJS, d3) {
+  var lineUpDemoConfig = {
+    renderingOptions: {
+      stacked: true,
+      histograms: true
+    }
+  };
+
+  var lineupItems = null;
+  var lineupFeatures = null;
+
+  d3.select(window).on('resize', function() {
+    if (lineupItems) {
+      lineupItems.update()
+    }
+    if (lineupFeatures) {
+      lineupFeatures.update()
+    }
+  });
+
+  function fixMissing(columns, data) {
+    columns.forEach(function(col){
+      if (col.type === 'number' && !col.domain) {
+        var old = col.domain || [NaN, NaN];
+        var minmax = d3.extent(data, function (row) { return row[col.column].length === 0 ? undefined : +(row[col.column])});
+        col.domain = [
+          isNaN(old[0]) ? minmax[0] : old[0],
+          isNaN(old[1]) ? minmax[1] : old[1]
+        ];
+      } else if (col.type === 'categorical' && !col.categories) {
+        var sset = d3.set(data.map(function (row) { return row[col];}));
+        col.categories = sset.values().sort();
+      }
+    });
+  }
+
+  function transpose(data, columns) {
+    var name = columns.shift();
+    return columns.map(function(d, i) {
+      var r = {};
+      r.name = d;
+      data.forEach(function(row) {
+        r[row[name]] = row[d];
+      });
+      return r;
+    });
+  }
+
+  function link(lineup, columns) {
+    var provider = lineup.data;
+    return function(indices) {
+      var r = provider.getLastRanking();
+      var toShow = indices.map(function(d) { return columns[d+1]});
+      var toRemove = r.columns.filter(function(d, i) {
+        if (d.desc.type === 'rank' || d.desc === columns[0] || d.desc.type === 'selection') {
+          return false; //keep nam
+        }
+        var k = toShow.indexOf(d.desc);
+        if (k < 0) {
+          return true;
+        }
+        toShow.splice(k, 1);
+        return false;
+      });
+      toRemove.forEach(r.remove.bind(r));
+      var new_columns = toShow.map(provider.push.bind(provider, r));
+      if (new_columns.length > 0) {
+        new_columns[0].sortByMe();
+      }
+    };
+  }
+
+  function loadDataImpl(name, desc, _data) {
+    fixMissing(desc.columns, _data);
+    LineUpJS.deriveColors(desc.columns);
+
+    var _transposed = transpose(_data, Object.keys(_data[0]));
+    var transposedColumns = deriveDesc(Object.keys(_transposed[0]), _transposed).columns;
+    LineUpJS.deriveColors(transposedColumns);
+
+  	var provider = LineUpJS.createLocalStorage(_data, desc.columns);
+    var r1 = provider.pushRanking();
+    //provider.push(r1, LineUpJS.model.createSelectionDesc());
+    provider.push(r1, desc.columns[0]);
+    var providerF = LineUpJS.createLocalStorage(_transposed, transposedColumns);
+    var r2 = providerF.pushRanking();
+    //provider.push(r2, LineUpJS.model.createSelectionDesc());
+    providerF.push(r2, transposedColumns[0]);
+
+    lineupItems = LineUpJS.create(provider, d3.select('#item_table'), lineUpDemoConfig);
+    lineupFeatures = LineUpJS.create(providerF, d3.select('#feature_table'), lineUpDemoConfig);
+
+    //TODO link them together
+    lineupItems.on('multiSelectionChanged', link(lineupFeatures, transposedColumns));
+    lineupFeatures.on('multiSelectionChanged', link(lineupItems, desc.columns));
+
+  	lineupItems.update();
+  	lineupFeatures.update();
+  }
+
+
+  function countOccurrences(text, char) {
+    return (text.match(new RegExp(char,'g'))||[]).length;
+  }
+
+  function isNumeric(obj) {
+    return (obj - parseFloat(obj) + 1) >= 0;
+  }
+
+  function deriveDesc(columns, data, separator) {
+    var cols = columns.map(function(col) {
+      var r = {
+        label: col,
+        column: col,
+        type: 'string'
+      };
+      if (isNumeric(data[0][col])) {
+        r.type = 'number';
+        r.domain = d3.extent(data, function (row) { return row[col].length === 0 ? undefined : +(row[col])});
+      } else {
+        var sset = d3.set(data.map(function (row) { return row[col];}));
+        if (sset.size() <= Math.max(20, data.length * 0.2)) { //at most 20 percent unique values
+          r.type = 'categorical';
+          r.categories = sset.values().sort();
+        }
+      }
+      return r;
+    });
+    return {
+      separator: separator,
+      primaryKey : columns[0],
+      columns: cols
+    };
+  }
+
+  function normalizeValue(val) {
+    if (typeof val === 'string') {
+      val = val.trim();
+      if (val.length >= 2 && val.charAt(0) === '"' && val.charAt(val.length-1) === '"') {
+        val = val.slice(1, val.length-1);
+      }
+    }
+    return val;
+  }
+  /**
+   * trims the given object
+   * @param row
+   * @return {{}}
+   */
+  function normalizeRow(row) {
+    var r = {};
+    Object.keys(row).forEach(function (key) {
+      r[normalizeValue(key)] = normalizeValue(row[key]);
+    });
+    return r;
+  }
+
+  function loadDataFromStructuredText(headers, _data, fileName) {
+    //derive a description file
+    var desc = deriveDesc(headers, _data);
+    var name = fileName.substring(0, fileName.lastIndexOf('.'));
+    loadDataImpl(name, desc, _data);
+  }
+
+  function loadDataFileFromText(data_s, fileName) {
+    var header = data_s.slice(0,data_s.indexOf('\n'));
+    var _data = d3.dsv(';', 'text/plain').parse(data_s, normalizeRow);
+    var headers = header.split(';').map(normalizeValue);
+    loadDataFromStructuredText(headers, _data, fileName);
+  }
+
+  var url = 'data/mtcars.csv';
+  //access the url using get request and then parse the data file
+  //d3.text(url, 'text/plain', function(data) {
+  //    loadDataFileFromText(data, 'wur2013');
+  //});
+  //access the url using get request and use d3.tsv since it is an tab separated file
+  d3.csv(url, function(data) {
+    loadDataFromStructuredText(Object.keys(data[0]), data, 'Cars Dataset');
+  });
+
+}(LineUpJS, d3));
